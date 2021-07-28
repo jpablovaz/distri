8431051513d7cb41ee9cb9a37841becd6fe36f26
Juan Vazquez <jpablo.vaz@gmail.com>
2021-07-28 23:26:00 +0200

    wget

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -4,7 +4,6 @@
 #######################################################
 yes | sudo apt update
 yes | sudo apt upgrade
-yes | sudo apt install git
 yes | sudo apt install docker.io
 yes | sudo apt install docker-compose
 
@@ -12,14 +11,32 @@ yes | sudo apt install docker-compose
 # Cleaning up old installations
 #######################################################
 sudo rm /opt/distri -R
+
 sudo mkdir /opt/distri
+sudo mkdir /opt/distri/scripts
+sudo mkdir /opt/distri/app
+sudo mkdir /opt/distri/docker
+sudo mkdir /opt/distri/docker/java
+sudo mkdir /opt/distri/backups
+sudo mkdir /opt/distri/cache
+
 sudo chmod 777 /opt/distri -R
 
 #######################################################
 # Downloads
 #######################################################
-git clone https://github.com/jpablovaz/distri.git /opt/distri
+wget /opt/distri/scripts/ https://raw.githubusercontent.com/jpablovaz/distri/main/scripts/distri.service
+wget /opt/distri/scripts/ https://raw.githubusercontent.com/jpablovaz/distri/main/scripts/booting.sh
 
+wget /opt/distri/docker/ https://raw.githubusercontent.com/jpablovaz/distri/main/docker/docker-compose.yml
+wget /opt/distri/docker/java/ https://raw.githubusercontent.com/jpablovaz/distri/main/docker/java/Dockerfile
+wget /opt/distri/docker/java/ https://github.com/jpablovaz/distri/raw/main/docker/java/distri.jar
+
+wget /opt/distri/docker/app/ https://github.com/jpablovaz/distri/raw/main/app/app.aa
+wget /opt/distri/docker/app/ https://github.com/jpablovaz/distri/raw/main/app/app.ab
+wget /opt/distri/docker/app/ https://github.com/jpablovaz/distri/raw/main/app/app.ac
+wget /opt/distri/docker/app/ https://github.com/jpablovaz/distri/raw/main/app/app.ad
+ 
 #######################################################
 # Setting Up Boot Script
 #######################################################
@@ -27,12 +44,6 @@ sudo chmod +x /opt/distri/scripts/booting.sh
 sudo mv /opt/distri/scripts/distri.service /etc/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl enable distri.service
-
-#######################################################
-# Extras
-#######################################################
-mkdir /opt/distri/backups
-mkdir /opt/distri/cache
 
 #######################################################
 # Re-Join APP
