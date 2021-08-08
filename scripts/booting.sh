f1a9cce8a4ea304df434abbe7ab67b32ad6dda50
Juan Vazquez <jpablo.vaz@gmail.com>
2021-08-08 19:58:53 +0200

    versions

--- scripts/booting.sh
+++ scripts/booting.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
+
+echo "LOCAL:"
+app_local_version="`wget -qO- http://localhost:8090/app-version`"
+api_local_version="`wget -qO- http://localhost:8090/api-version`"
+echo $app_local_version
+echo $api_local_version
+
+echo "CLOUD:"
+app_cloud_version=`cat app_cloud_version.txt`
+api_cloud_version=`cat api_cloud_version.txt`
+echo $app_cloud_version
+echo $api_cloud_version
+
+if [ "$app_local_version" = "$app_cloud_version" ]; then
+    echo 'Be careful not to fall off!'
+else
+    echo 'Be careful not to fall off!'
+fi
+
+if [ "$api_local_version" = "$api_cloud_version" ]; then
+    echo 'Be careful not to fall off!'
+else
+    echo 'Be careful not to fall off!'
+fi
+
+wget https://raw.githubusercontent.com/jpablovaz/distri/main/scripts/booting.sh -P /opt/distri/scripts/ 
 cd /opt/distri/docker
 docker-compose up --build
