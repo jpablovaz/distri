46dbd7d7dde9da0b65d7a3deabf97a39f124bdc3
Juan Vazquez <jpablo.vaz@gmail.com>
2021-08-10 04:49:56 +0200

    update

--- scripts/booting.sh
+++ scripts/booting.sh
@@ -7,9 +7,8 @@ wget https://raw.githubusercontent.com/jpablovaz/distri/main/scripts/booting.sh 
 wget https://raw.githubusercontent.com/jpablovaz/distri/main/docker/docker-compose.yml -P /opt/distri/docker/docker-compose2.yml
 wget https://raw.githubusercontent.com/jpablovaz/distri/main/docker/java/Dockerfile -P /opt/distri/docker/java/Dockerfile2
 
-mv /opt/distri/scripts/booting2.sh /opt/distri/scripts/booting.sh
-mv /opt/distri/docker/docker-compose2.yml /opt/distri/docker/docker-compose.yml
-mv /opt/distri/docker/java/Dockerfile2 /opt/distri/docker/java/Dockerfile
+wget https://raw.githubusercontent.com/jpablovaz/distri/main/version/app_version.txt -P /opt/distri/version/app_version2.txt
+wget https://raw.githubusercontent.com/jpablovaz/distri/main/version/api_version.txt -P /opt/distri/version/api_version2.txt
 
 #######################################################
 # Reading cloud version
@@ -32,25 +31,21 @@ echo $api_version
 
 #yes | rm /opt/distri/version/app_version.txt
 #yes | rm /opt/distri/version/api_version.txt
-wget https://raw.githubusercontent.com/jpablovaz/distri/main/version/app_version.txt -P /opt/distri/version/app_version2.txt
-wget https://raw.githubusercontent.com/jpablovaz/distri/main/version/api_version.txt -P /opt/distri/version/api_version2.txt
-mv /opt/distri/version/app_version2.txt /opt/distri/version/app_version.txt
-mv /opt/distri/version/api_version2.txt /opt/distri/version/api_version.txt
-
 #######################################################
 # Reading local version
 #######################################################
 if [ "$app_version" != "$app_cloud_version" ]; then
     yes | rm /opt/distri/app/*
-    yes | rm /home/$user_dir/Desktop/Distribuidora_Mendoza.AppImage
+#    yes | rm /home/$user_dir/Desktop/Distribuidora_Mendoza.AppImage
     wget https://github.com/jpablovaz/distri/raw/main/app/app.aa -P /opt/distri/app/
     wget https://github.com/jpablovaz/distri/raw/main/app/app.ab -P /opt/distri/app/
     wget https://github.com/jpablovaz/distri/raw/main/app/app.ac -P /opt/distri/app/
     wget https://github.com/jpablovaz/distri/raw/main/app/app.ad -P /opt/distri/app/
     wget https://github.com/jpablovaz/distri/raw/main/app/app.ae -P /opt/distri/app/
     wget https://github.com/jpablovaz/distri/raw/main/app/app.af -P /opt/distri/app/
-    cat /opt/distri/app/app.* > /home/$user_dir/Desktop/Distribuidora_Mendoza.AppImage
-    chmod +x /home/$user_dir/Desktop/Distribuidora_Mendoza.AppImage
+#    cat /opt/distri/app/app.* > /home/$user_dir/Desktop/Distribuidora_Mendoza.AppImage
+    cat /opt/distri/app/app.* > /opt/distri/app/dm.AppImage
+    chmod +x /opt/distri/app/dm.AppImage
     mkdir /home/$user_dir/Desktop/app_Diff
 else
     mkdir /home/$user_dir/Desktop/app_Equal
@@ -63,6 +58,15 @@ if [ "$api_version" != "$api_cloud_version" ]; then
 else
     mkdir /home/$user_dir/Desktop/api_Equal
 fi
+
+mv /opt/distri/scripts/booting2.sh /opt/distri/scripts/booting.sh
+chmod +x /opt/distri/app/dm.AppImage
+mv /opt/distri/docker/docker-compose2.yml /opt/distri/docker/docker-compose.yml
+mv /opt/distri/docker/java/Dockerfile2 /opt/distri/docker/java/Dockerfile
+mv /opt/distri/version/app_version2.txt /opt/distri/version/app_version.txt
+mv /opt/distri/version/api_version2.txt /opt/distri/version/api_version.txt
+
+
 
 cd /opt/distri/docker
 docker-compose up
