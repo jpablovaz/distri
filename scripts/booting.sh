bb9736a4b7fb729bdfe0027b2a6b32cf9e11a6eb
Juan Vazquez <jpablo.vaz@gmail.com>
2021-08-12 10:18:01 +0200

    update

--- scripts/booting.sh
+++ scripts/booting.sh
@@ -12,7 +12,6 @@ temp_app_version=$temp/app_version.txt
 temp_api_version=$temp/api_version.txt
 wget https://raw.githubusercontent.com/jpablovaz/distri/main/version/app_version.txt -P $temp
 wget https://raw.githubusercontent.com/jpablovaz/distri/main/version/api_version.txt -P $temp
-
 if [ -f $temp_app_version ]; then
     app_cloud_version=`cat $temp_app_version`
     rm $temp_app_version
@@ -30,22 +29,17 @@ else
 fi
 
 #######################################################
-# Files
+# Download Files
 #######################################################
-booting=/opt/distri/scripts/booting.sh
 compose=/opt/distri/docker/docker-compose.yml
 docker=/opt/distri/docker/java/Dockerfile
-
 temp_compose=$temp/docker-compose.yml
 temp_docker=$temp/Dockerfile
-
 wget https://raw.githubusercontent.com/jpablovaz/distri/main/docker/docker-compose.yml -P $temp
 wget https://raw.githubusercontent.com/jpablovaz/distri/main/docker/java/Dockerfile -P $temp
-
 if [ -f $temp_compose ]; then
     mv $temp_compose $compose
 fi
-
 if [ -f $temp_docker ]; then
     mv $temp_docker $docker
 fi
@@ -63,26 +57,8 @@ if [ $app_version != $app_cloud_version ]; then
     wget https://github.com/jpablovaz/distri/raw/main/app/app.af -P /opt/distri/app/
     cat /opt/distri/app/app.* > /home/$username/.distri/dm.AppImage
     chmod +x /home/$username/.distri/dm.AppImage
-    if [ -f "/home/$username/Desktop/app_Diff" ]; then
-        echo "Already exist Juancito"
-    else
-        mkdir /home/$username/Desktop/app_Diff
-    fi
-else
-    if [ -f "/home/$username/Desktop/app_Equal" ]; then
-        echo "Already exist Juancito"
-    else
-        mkdir /home/$username/Desktop/app_Equal
-    fi
-fi
-
 if [ $api_version != $api_cloud_version ]; then
     rm /opt/distri/docker/java/distri.jar
     wget https://github.com/jpablovaz/distri/raw/main/docker/java/distri.jar -P /opt/distri/docker/java/
-    mkdir /home/$username/Desktop/api_Diff
-else
-    mkdir /home/$username/Desktop/api_Equal
-fi
-
 cd /opt/distri/docker
 docker-compose up
