edc2252ff575abfc452f399f95ee7e5a301410d8
Juan Vazquez <jpablo.vaz@gmail.com>
2021-08-12 02:38:51 +0200

    update

--- scripts/booting.sh
+++ scripts/booting.sh
@@ -61,7 +61,7 @@ if [ $app_version != $app_cloud_version ]; then
     wget https://github.com/jpablovaz/distri/raw/main/app/app.ad -P /opt/distri/app/
     wget https://github.com/jpablovaz/distri/raw/main/app/app.ae -P /opt/distri/app/
     wget https://github.com/jpablovaz/distri/raw/main/app/app.af -P /opt/distri/app/
-    cat /opt/distri/app/app.* > /opt/distri/app/dm.AppImage
+    cat /opt/distri/app/app.* > /home/$username/.distri/dm.AppImage
     chmod +x /opt/distri/app/dm.AppImage
     if [ -f "/home/$username/Desktop/app_Diff" ]; then
         echo "Already exist Juancito"
