a6dae872f24cd0e18d3168777cbf8800ba4be62d
Juan Vazquez <jpablo.vaz@gmail.com>
2022-06-16 17:28:22 +0200

    updated

--- scripts/booting.sh
+++ scripts/booting.sh
@@ -77,6 +77,6 @@ cd /opt/distri/docker
 
 rm $running
 
-sleep 40 docker-compose up &
-sleep 45 sh /opt/distri/scripts/print.sh &
+sleep 40 && docker-compose up &
+sleep 45 && sh /opt/distri/scripts/print.sh &
 #/home/$username/.distri/dm.AppImage &
