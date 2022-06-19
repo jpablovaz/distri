9350451f60f6d4a63dcc2a6ef920df37d59f941e
Juan Vazquez <jpablo.vaz@gmail.com>
2022-06-19 13:33:24 +0200

    updated

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -32,12 +32,12 @@ sudo mkdir /opt/distri/cache
 sudo mkdir ~/.local/share/applications
 sudo touch /opt/distri/scripts/crontab/dcron
 dcron=/opt/distri/scripts/crontab/dcron
-echo \@reboot sleep 1 \; sh  /opt/distri/scripts/crontab/_init.sh >> $dcron
-echo \@reboot sleep 7 \; sh  /opt/distri/scripts/crontab/updater.sh >> $dcron
-echo \@reboot sleep 18 \; sh /opt/distri/scripts/crontab/runner/back_end.sh >> $dcron
-echo \@reboot sleep 18 \; sh /opt/distri/scripts/crontab/runner/front_end.sh >> $dcron
-echo \@reboot sleep 18 \; sh /opt/distri/scripts/crontab/runner/printer.sh >> $dcron
-echo \@reboot sleep 30 \; sh /home/$username/.distri/desktop.sh >> $dcron
+sudo echo \@reboot sleep 1 \; sh  /opt/distri/scripts/crontab/_init.sh >> $dcron
+sudo echo \@reboot sleep 7 \; sh  /opt/distri/scripts/crontab/updater.sh >> $dcron
+sudo echo \@reboot sleep 18 \; sh /opt/distri/scripts/crontab/runner/back_end.sh >> $dcron
+sudo echo \@reboot sleep 18 \; sh /opt/distri/scripts/crontab/runner/front_end.sh >> $dcron
+sudo echo \@reboot sleep 18 \; sh /opt/distri/scripts/crontab/runner/printer.sh >> $dcron
+sudo echo \@reboot sleep 30 \; sh /home/$username/.distri/desktop.sh >> $dcron
 crontab $dcron
 
 sudo chmod 777 /opt/distri -R
