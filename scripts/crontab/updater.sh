cbee20a13143bc8cb73c2c085a41ec1e6d9f3d49
Juan Vazquez <jpablo.vaz@gmail.com>
2022-06-19 12:20:21 +0200

    updated

--- scripts/crontab/updater.sh
+++ scripts/crontab/updater.sh
@@ -15,12 +15,12 @@ dcron=$base/dcron
 #chmod 777 $dcron
 #fi
 > $dcron
-echo \@reboot sleep 10 \; sh /opt/distri/scripts/crontab/_init.sh >> $dcron
-echo \@reboot sleep 60 \; sh /opt/distri/scripts/crontab/updater.sh >> $dcron
-echo \@reboot sleep 122 \; sh /opt/distri/scripts/crontab/runner/back_end.sh >> $dcron
-echo \@reboot sleep 123 \; sh /opt/distri/scripts/crontab/runner/front_end.sh >> $dcron
-echo \@reboot sleep 120 \; sh /opt/distri/scripts/crontab/runner/printer.sh >> $dcron
-echo \@reboot sleep 130 \; sh /home/$username/.distri/desktop.sh >> $dcron
+echo \@reboot sleep 1 \; sh /opt/distri/scripts/crontab/_init.sh >> $dcron
+echo \@reboot sleep 7 \; sh /opt/distri/scripts/crontab/updater.sh >> $dcron
+echo \@reboot sleep 18 \; sh /opt/distri/scripts/crontab/runner/back_end.sh >> $dcron
+echo \@reboot sleep 18 \; sh /opt/distri/scripts/crontab/runner/front_end.sh >> $dcron
+echo \@reboot sleep 18 \; sh /opt/distri/scripts/crontab/runner/printer.sh >> $dcron
+echo \@reboot sleep 30 \; sh /home/$username/.distri/desktop.sh >> $dcron
 crontab $dcron
 ################################################
 # back_end
