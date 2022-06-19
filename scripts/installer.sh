9f0a14f13224ed49580faa8623af18ebe746614f
Juan Vazquez <jpablo.vaz@gmail.com>
2022-06-19 15:31:02 +0200

    updated

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -30,6 +30,7 @@ sudo mkdir /opt/distri/backups
 sudo mkdir /opt/distri/version
 sudo mkdir /opt/distri/cache
 sudo mkdir ~/.local/share/applications
+sudo touch ~/.local/share/applications/distri.desktop
 sudo chmod 777 /opt/distri -R
 
 dcron=/opt/distri/scripts/crontab/dcron
