a55cc5a667ac0328fa4c4243cf74cc6e23a0e097
Juan Vazquez <jpablo.vaz@gmail.com>
2022-07-14 13:52:59 +0200

    updated

--- scripts/crontab/runner/ip_reader.sh
+++ scripts/crontab/runner/ip_reader.sh
@@ -4,6 +4,6 @@ echo > $path/ip_me.data
 while read p; do
 	p=$(echo $p | awk '{print $2 }')
 	var=$(/sbin/ifconfig | grep $p | awk '{print $2 }')
-	echo $var >> $path/ip_me.data
-	#[ -z $var ] || echo $var > $path/ip_me.data
+	#echo $var >> $path/ip_me.data
+	[ -z $var ] || echo $var > $path/ip_me.data
 done <$path/ip_network.data
