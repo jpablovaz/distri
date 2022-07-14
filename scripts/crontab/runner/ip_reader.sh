09678d4ecc19552fc0203258cf91a272929000d9
Juan Vazquez <jpablo.vaz@gmail.com>
2022-07-14 13:30:55 +0200

    updated

--- scripts/crontab/runner/ip_reader.sh
+++ scripts/crontab/runner/ip_reader.sh
@@ -1,11 +1,10 @@
+#!/bin/bash
 path=/opt/distri/docker/java
-#echo "holaY" > $path/ip_meY.data
-#echo "holaX" > $path/ip_meX.data
 echo > $path/ip_me.data
 while read p; do
 	p=$(echo $p | awk '{print $2 }')
-	#var=$(ifconfig | grep "$p" | awk '{print $2 }')
-	var=$(ifconfig | grep 1)
-	echo $var >> $path/ip_me.data
-	#[ -z $var ] || echo $var > $path/ip_me.data
+	var=$(ifconfig | grep "$p" | awk '{print $2 }')
+	#var=$(ifconfig | grep 1)
+	#echo $var >> $path/ip_me.data
+	[ -z $var ] || echo $var > $path/ip_me.data
 done <$path/ip_network.data
