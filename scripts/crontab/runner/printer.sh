503af83b6ee53a7fa3242bb1527b01a0ad2dd15c
Juan Vazquez <jpablo.vaz@gmail.com>
2022-07-29 20:03:03 +0200

    updated

--- scripts/crontab/runner/printer.sh
+++ scripts/crontab/runner/printer.sh
@@ -3,51 +3,49 @@ username=`cat /opt/distri/scripts/user.txt`
 
 java_path=/opt/distri/docker/java/pdf/
 pdf_doc_path=/home/$username/Escritorio/Documentos_PDF/
-distri_path=/home/$username/Escritorio/Distribuidora/
 list_path=/home/$username/Escritorio/Listas/
 pdf_list_path=/home/$username/Escritorio/Listas/PDF/
 csv_list_path=/home/$username/Escritorio/Listas/CSV/
 
+# JAVA
 rm -f $java_path -R
 mkdir $java_path
+chmod 777 $java_path -R
 
+# DOCUMENTS
 rm -f $pdf_doc_path -R
 mkdir $pdf_doc_path
+chmod 777 $pdf_doc_path -R
 
+# LIST ROOT
 rm -f $list_path -R
 mkdir $list_path
+chmod 777 $list_path -R
 
+# LIST PDF
 rm -f $pdf_list_path -R
 mkdir $pdf_list_path
+chmod 777 $pdf_list_path -R
 
+# LIST CSV
 rm -f $csv_list_path -R
 mkdir $csv_list_path
-
-rm -f $distri_path -R
-mkdir $distri_path
-
-chmod 777 $java_path -R
-chmod 777 $pdf_doc_path -R
-chmod 777 $list_path -R
-chmod 777 $pdf_list_path -R
 chmod 777 $csv_list_path -R
-chmod 777 $distri_path -R
 
+# LIST FOLDER 1..12
 n=0
-#while [ "$n" -lt 12 ] && [ ! -e filename ]; do
-#    n=$(( n + 1 ))
-#	pre=$n
-#	if [ "$n" -lt 10 ]; then  pre='0'${n} fi
-#
-#	rm -f $pdf_list_path$pre -R
-#	mkdir $pdf_list_path$pre
-#	chmod 777 $pdf_list_path$pre -R
-#
-#	rm -f $csv_list_path$pre -R
-#	mkdir $csv_list_path$pre
-#	chmod 777 $csv_list_path$pre -R
-#
-#done
+while [ $n -lt 12 ]; do
+    n=$(( n + 1 ))
+	[ $n -lt 10 ] && folder=0$n || folder=$n
+
+	rm -f $pdf_list_path$folder -R
+	mkdir $pdf_list_path$folder
+	chmod 777 $pdf_list_path$folder -R
+
+	rm -f $csv_list_path$folder -R
+	mkdir $csv_list_path$folder
+	chmod 777 $csv_list_path$folder -R
+done
 
 while true
 do
@@ -58,12 +56,11 @@ do
 		pdf=`echo $f | cut -b 2-2`
 		p1=`echo $f | cut -b 3-3`
 		p2=`echo $f | cut -b 4-4`
-		nu=`echo $f | cut -b 3-4`
+		num=`echo $f | cut -b 3-4`
 		name=`echo $f | cut -b 5-100`
 		if [ $at = '@' ]; then
 			if [ $pdf = '1' ]; then
-				#cp $java_path$f $pdf_doc_path$name
-				cp $java_path$f $distri_path$name
+				cp $java_path$f $pdf_doc_path$name
 			fi
 			if [ $p1 = '1' ]; then
 				lp $java_path$f
@@ -72,16 +69,15 @@ do
 				lp $java_path$f
 			fi
 			rm -f $java_path$f
-			#chmod 777 $pdf_doc_path$name
-			chmod 777 $distri_path$name
-		#elif [ $at = 'p' ]; then
-		#	cp $java_path$f $pdf_list_path$nu/$name
-		#	rm -f $java_path$f
-		#	chmod 777 $pdf_list_path$nu/$name
-		#elif [ $at = 'c' ]; then
-		#	cp $java_path$f $csv_list_path$nu/$name
-		#	rm -f $java_path$f
-		#	chmod 777 $csv_list_path$nu/$name
+			chmod 777 $pdf_doc_path$name
+		elif [ $at = '&' ]; then
+			cp $java_path$f $pdf_list_path$nu/$name
+			rm -f $java_path$f
+			chmod 777 $pdf_list_path$nu/$name
+		elif [ $at = '#' ]; then
+			cp $java_path$f $csv_list_path$nu/$name
+			rm -f $java_path$f
+			chmod 777 $csv_list_path$nu/$name
 		fi
 	done
 done
