c415d89ffa6a831064179447ac3426178446148a
Juan Vazquez <juanvazquez@air.local>
2023-01-07 22:33:33 +0100

    up

--- scripts/crontab/runner/front_end.sh
+++ scripts/crontab/runner/front_end.sh
@@ -39,6 +39,10 @@ if [ $front_end_version != $front_end_cloud_version ]; then
     wget $git_files/front_end.af -P /opt/distri/front_end/
     wget $git_files/front_end.ag -P /opt/distri/front_end/
     cat /opt/distri/front_end/front_end.* > /home/$username/.distri/dm.AppImage
+
+    rm /opt/distri/docker/website -R
+    wget $git_files/docker/build.tar.bz2 -P /opt/distri/docker/
+    tar -jxvf /opt/distri/docker/archive_name.tar.bz2 -C /opt/distri/docker/build/
 fi
 chmod +x /home/$username/.distri/dm.AppImage
 ########################################################
