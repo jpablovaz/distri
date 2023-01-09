8344f0544f7b4c6954f7f1e68bc16d065c8910bf
Juan Vazquez <juanvazquez@air.local>
2023-01-09 16:10:15 +0100

    up

--- scripts/crontab/runner/front_end.sh
+++ scripts/crontab/runner/front_end.sh
@@ -6,13 +6,6 @@ github=https://raw.githubusercontent.com/jpablovaz/distri/main
 git_files=https://github.com/jpablovaz/distri/raw/main/front_end
 username=`cat /opt/distri/scripts/user.txt`
 temp=/opt/distri/scripts/temp
-running=/opt/distri/scripts/running
-#######################################################
-# Update in process Flag On
-#######################################################
-if ! [ -f $running ]; then
-    touch $running
-fi
 #######################################################
 # Getting Version number
 #######################################################
@@ -30,29 +23,8 @@ fi
 # Download new Version
 #######################################################
 if [ $front_end_version != $front_end_cloud_version ]; then
-    rm /opt/distri/front_end/*
-    wget $git_files/front_end.aa -P /opt/distri/front_end/
-    wget $git_files/front_end.ab -P /opt/distri/front_end/
-    wget $git_files/front_end.ac -P /opt/distri/front_end/
-    wget $git_files/front_end.ad -P /opt/distri/front_end/
-    wget $git_files/front_end.ae -P /opt/distri/front_end/
-    wget $git_files/front_end.af -P /opt/distri/front_end/
-    wget $git_files/front_end.ag -P /opt/distri/front_end/
-    cat /opt/distri/front_end/front_end.* > /home/$username/.distri/dm.AppImage
-
     rm /opt/distri/docker/build -R
     wget $git_files/build.tar.bz2 -P /opt/distri/docker/
-#    tar -jxvf /opt/distri/docker/archive_name.tar.bz2 -C /opt/distri/docker/
     tar -jxvf /opt/distri/docker/build.tar.bz2 -C /opt/distri/docker/
     rm /opt/distri/docker/build.tar.bz2
 fi
-chmod +x /home/$username/.distri/dm.AppImage
-########################################################
-# Update in process Flag Off
-########################################################
-rm $running
-########################################################
-# Run the App
-########################################################
-#/home/$username/.distri/dm.AppImage
-#sh /home/$username/.distri/desktop.sh
--- scripts/crontab/updater.sh
+++ scripts/crontab/updater.sh
@@ -8,24 +8,12 @@ username=`cat /opt/distri/scripts/user.txt`
 ################################################
 # dcron
 dcron=$base/dcron
-#temp_dcron=$temp/dcron
-#wget $github/dcron -P $temp
-#if [ -f $temp_dcron ]; then
-#	rm -f $dcron
-#    mv $temp_dcron $dcron
-#chmod 777 $dcron
-#fi
 > $dcron
-
-#echo \@reboot sleep 15 \; sh /opt/distri/scripts/crontab/runner/ip_reader.sh >> $dcron
 echo \@reboot sleep 20 \; sh /opt/distri/scripts/crontab/_init.sh >> $dcron
-echo \@reboot sleep 33 \; sh /opt/distri/scripts/crontab/updater.sh >> $dcron 
-echo \@reboot sleep 38 \; sh /opt/distri/scripts/crontab/runner/back_end.sh >> $dcron
-echo \@reboot sleep 38 \; sh /opt/distri/scripts/crontab/runner/front_end.sh >> $dcron
-echo \@reboot sleep 38 \; sh /opt/distri/scripts/crontab/runner/printer.sh >> $dcron
-echo \@reboot sleep 45 \; sh /home/$username/.distri/desktop.sh >> $dcron
-#echo \@reboot sleep 18 \; sh /opt/distri/scripts/crontab/runner/once.sh >> $dcron
-#echo \@reboot sleep 18 \; sh /opt/distri/scripts/crontab/runner/backup.sh >> $dcron
+echo \@reboot sleep 20 \; sh /opt/distri/scripts/crontab/updater.sh >> $dcron 
+echo \@reboot sleep 20 \; sh /opt/distri/scripts/crontab/runner/front_end.sh >> $dcron
+echo \@reboot sleep 30 \; sh /opt/distri/scripts/crontab/runner/back_end.sh >> $dcron
+echo \@reboot sleep 30 \; sh /opt/distri/scripts/crontab/runner/printer.sh >> $dcron
 crontab $dcron
 ################################################
 # back_end
@@ -54,39 +42,3 @@ if [ -f $temp_printer ]; then
     mv $temp_printer $printer
 	chmod +x $printer
 fi
-################################################
-# ip_reader
-#ip_reader=$base/runner/ip_reader.sh
-#temp_ip_reader=$temp/ip_reader.sh
-#wget $github/runner/ip_reader.sh -P $temp
-#if [ -f $temp_ip_reader ]; then
-#    mv $temp_ip_reader $ip_reader
-#	chmod +x $ip_reader
-#fi
-################################################
-# ip_network
-#ip_network=$base_java/ip_network.data
-#temp_ip_network=$temp/ip_network.data
-#wget $github_java/ip_network.data -P $temp
-#if [ -f $temp_ip_network ]; then
-#    mv $temp_ip_network $ip_network
-#	chmod +x $ip_network
-#fi
-################################################
-# once
-#once=$base/runner/once.sh
-#temp_once=$temp/once.sh
-#wget $github/runner/once.sh -P $temp
-#if [ -f $temp_once ]; then
-#    mv $temp_once $once
-#	chmod +x $once
-#fi
-################################################
-# backup
-#backup=$base/runner/backup.sh
-#temp_backup=$temp/backup.sh
-#wget $github/runner/backup.sh -P $temp
-#if [ -f $temp_backup ]; then
-#    mv $temp_backup $backup
-#	chmod +x $backup
-#fi
