1eb1b0652a4f997b01b260db208af2134baa3078
Juan Vazquez <juanvazquez@air.local>
2023-01-16 20:00:37 +0100

    up

--- scripts/crontab/runner/backup.sh
+++ scripts/crontab/runner/backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 username=`cat /opt/distri/scripts/user.txt`
 backup_java_path=/opt/distri/docker/java/backup/
-backup_dropbox_path=/home/$username/Dropbox/Backs/
+backup_dropbox_path=/home/$username/Dropbox/Backups/
 while true
 do
 	sleep 100
