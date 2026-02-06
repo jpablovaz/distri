4083722a73f53f02e9b6e716cd59521861789b71
Juan Project <juan@project.com>
2026-02-06 19:01:30 +0100

    fix: backups

--- scripts/crontab/runner/backup.sh
+++ scripts/crontab/runner/backup.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
+#---------------------------------------------------
+DAY_OF_THE_WEEK=$(date +%A)
+DAY_OF_THE_MONTH=$(date +%F)
+#---------------------------------------------------
 username=`cat /opt/distri/scripts/user.txt`
 backup_java_path=/opt/distri/docker/java/backup/
-backup_dropbox_path=/home/$username/Dropbox/Backups/
+backup_dropbox_path=/home/$username/Dropbox/Backups/$DAY_OF_THE_WEEK
+backup_desktop_path=/home/$username/Desktop/Backups/up-$DAY_OF_THE_MONTH
+#---------------------------------------------------
 while true
 do
-	sleep 100
+  sleep 100
   if [ -f $backup_java_path/delete.txt ]; then
     rm $backup_java_path/delete.txt
     rm $backup_dropbox_path/* -f
+    rm $backup_desktop_path/* -f
   fi
-  cp -R -u -p $backup_java_path/* $backup_dropbox_path
+  #cp -R -u -p $backup_java_path/* $backup_dropbox_path
+  cp -R -u $backup_java_path/* $backup_dropbox_path
+  cp -R -u $backup_java_path/* $backup_desktop_path
 done
